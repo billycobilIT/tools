3b59bd3de4d056547a83ba67fa2b49c01bc8a36f
ALFINKA <alfinkurniawan9727@gmail.com>
2019-12-21 18:51:09 +0700

    tools

---
+++ install.sh
@@ -0,0 +1,8 @@
+clear
+pkg install git -y wget -y nano -y - curl -y  php -y 
+apt update && apt upgrade
+git clone https://github.com/Rusmana-ID/rus
+pkg install python -y python2 -y bash -y 
+pip install --upgrade pip
+cd rus
+bash v2.sh
